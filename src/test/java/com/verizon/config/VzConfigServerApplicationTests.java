e3d88341a4c482bff2ca3a4abe7faeecfe225ecb
madhuri0409 <44791342+madhuri0409@users.noreply.github.com>
2020-05-31 19:35:27 -0500

    First Commit

---
+++ src/test/java/com/verizon/config/VzConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.verizon.config;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VzConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
